6561b49636c6d6acc7cecff6fe9652c699cf0bed
todbertuzzi <tod.bertuzzi@gmail.com>
2025-01-14 18:02:23 +0100

    enableIdentityWidget a true  in cinfiug

--- gatsby-config.js
+++ gatsby-config.js
@@ -91,7 +91,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-netlify-cms",
       options: {
-        enableIdentityWidget: false,
+        enableIdentityWidget: true,
       },
     },
     {
