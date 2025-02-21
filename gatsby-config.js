262a898d34e2df26460890c8eb1686736af07eb9
todbertuzzi <tod.bertuzzi@gmail.com>
2025-02-21 12:48:20 +0100

    fix seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,7 +118,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
-        policy: [{ userAgent: '*', disallow: '/' }],
+        policy: [{ userAgent: '*', allow: '/' }],
       },
     },
   ],
